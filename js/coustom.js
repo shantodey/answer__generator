4c5f7397267c25f2a99cf213c25a26000b3e75c2
shantodey <hlw4672@gmail.com>
2025-05-30 12:52:59 +0600

    Resolved conflicts

--- js/coustom.js
+++ js/coustom.js
@@ -1,14 +1,11 @@
 const myButton = document.querySelector('.div3'); 
-
 function Answer__generator() {
     let value = document.querySelector('.div2').value;
-    const outputDiv = document.getElementById('output');
-    
+    const outputDiv = document.getElementById('output');   
     if (value > 18) {
-        outputDiv.innerHTML = "you are an adult";
+        outputDiv.innerHTML = "You are an adult";
     } else {
-        outputDiv.innerHTML = "you are not an adult";
+        outputDiv.innerHTML = "You are not an adult";
     }
 }
-
 myButton.addEventListener('click', Answer__generator);
